f95b78bf0d9079c6ce70f544c3cb7a911add993f
PanchalKeyur0806 <panchalkeyur694@outlook.com>
2025-09-29 21:39:26 +0530

    added searching, sorting, pagination in contactusController file

--- controllers/contactusController.js
+++ controllers/contactusController.js
@@ -5,13 +5,55 @@ import { catchAsync } from "../utils/catchAsync.js";
 import { successMessage } from "../utils/successMessage.js";
 
 export const getallContacts = catchAsync(async (req, res, next) => {
-  const contacts = await ContactUs.find().select("-__v");
+  const { firstName, lastName, email, message, sort } = req.query;
+  const queryObj = {};
+
+  if (firstName) {
+    queryObj.firstName = { $regex: firstName, $options: "i" };
+  }
+
+  if (lastName) {
+    queryObj.lastName = { $regex: lastName, $options: "i" };
+  }
+
+  if (email) {
+    queryObj.email = { $regex: email, $options: "i" };
+  }
+
+  if (message) {
+    queryObj.message = { $regex: message, $options: "i" };
+  }
+
+  // sorting options
+  const sortOptions = {
+    new: "-createdAt",
+    old: "createdAt",
+  };
+  const sortKey = sortOptions[sort] || sortOptions.new;
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 5;
+  const skip = (page - 1) * limit;
+
+  const contacts = await ContactUs.find(queryObj)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+
+  const totalDocs = await ContactUs.countDocuments(queryObj);
+  const numOfPages = Math.ceil(totalDocs / limit);
 
   res.status(200).json({
     status: "success",
     length: contacts.length,
     message: "all contacts found",
-    contacts,
+    data: {
+      totalDocs,
+      currentPage: page,
+      numOfPages,
+      contacts,
+    },
   });
 });
 
